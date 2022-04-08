e46921a7b4df29c95d477d1dbf5fbe26b2e8ae94
Alexander Sporn <github@alexsporn.de>
2022-04-08 17:52:49 +0200

    - Properly parse config file

--- plugin/app/app.go
+++ plugin/app/app.go
@@ -1,6 +1,10 @@
 package app
 
 import (
+	"fmt"
+	"sort"
+	"strings"
+
 	flag "github.com/spf13/pflag"
 	"go.uber.org/dig"
 
@@ -10,7 +14,13 @@ import (
 )
 
 var (
+	// configs
 	nodeConfig = configuration.New()
+
+	// config file flags
+	configFilesFlagSet = flag.NewFlagSet("config_files", flag.ContinueOnError)
+	nodeCfgFilePath    = configFilesFlagSet.StringP(CfgConfigFilePathNodeConfig, "c", "config.json", "file path of the config file")
+
 	InitPlugin *node.InitPlugin
 )
 
@@ -30,26 +40,90 @@ func init() {
 
 func initialize(params map[string][]*flag.FlagSet, maskedKeys []string) (*node.InitConfig, error) {
 
-	flagSets, err := normalizeFlagSets(params)
+	configFlagSets, err := normalizeFlagSets(params)
 	if err != nil {
 		return nil, err
 	}
 
-	parseFlags(flagSets)
+	var flagSetsToParse = configFlagSets
+	flagSetsToParse["config_files"] = configFilesFlagSet
 
-	//TODO: load config file properly
-	if err := nodeConfig.LoadFlagSet(flagSets["nodeConfig"]); err != nil {
+	parseFlags(flagSetsToParse)
+
+	if err = loadCfg(configFlagSets); err != nil {
 		return nil, err
 	}
-
 	if err := logger.InitGlobalLogger(nodeConfig); err != nil {
 		panic(err)
 	}
+
+	printConfig(maskedKeys)
 
 	return &node.InitConfig{
 		EnabledPlugins:  nodeConfig.Strings(CfgNodeEnablePlugins),
 		DisabledPlugins: nodeConfig.Strings(CfgNodeDisablePlugins),
 	}, nil
+}
+
+// parses the configuration and initializes the global logger.
+func loadCfg(flagSets map[string]*flag.FlagSet) error {
+
+	if hasFlag(flag.CommandLine, CfgConfigFilePathNodeConfig) {
+		// node config file is only loaded if the flag was specified
+		if err := nodeConfig.LoadFile(*nodeCfgFilePath); err != nil {
+			return fmt.Errorf("loading config file failed: %w", err)
+		}
+	}
+
+	// load the flags to set the default values
+	if err := nodeConfig.LoadFlagSet(flagSets["nodeConfig"]); err != nil {
+		return err
+	}
+
+	// load the env vars after default values from flags were set (otherwise the env vars are not added because the keys don't exist)
+	if err := nodeConfig.LoadEnvironmentVars(""); err != nil {
+		return err
+	}
+
+	// load the flags again to overwrite env vars that were also set via command line
+	if err := nodeConfig.LoadFlagSet(flagSets["nodeConfig"]); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func hasFlag(flagSet *flag.FlagSet, name string) bool {
+	has := false
+	flagSet.Visit(func(f *flag.Flag) {
+		if f.Name == name {
+			has = true
+		}
+	})
+	return has
+}
+
+func getList(a []string) string {
+	sort.Strings(a)
+	return "\n   - " + strings.Join(a, "\n   - ")
+}
+
+// prints the loaded configuration, but hides sensitive information.
+func printConfig(maskedKeys []string) {
+	nodeConfig.Print(maskedKeys)
+
+	enablePlugins := nodeConfig.Strings(CfgNodeEnablePlugins)
+	disablePlugins := nodeConfig.Strings(CfgNodeDisablePlugins)
+
+	if len(enablePlugins) > 0 || len(disablePlugins) > 0 {
+		if len(enablePlugins) > 0 {
+			fmt.Printf("\nThe following plugins are enabled: %s\n", getList(enablePlugins))
+		}
+		if len(disablePlugins) > 0 {
+			fmt.Printf("\nThe following plugins are disabled: %s\n", getList(disablePlugins))
+		}
+		fmt.Println()
+	}
 }
 
 // adds the given flag sets to flag.CommandLine and then parses them.
